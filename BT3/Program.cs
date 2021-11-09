62cf70467fbec6a47e27b9a0d733361b0c74d6ca
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-11-09 20:33:30 +0700

    done

---
+++ BT3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double x;
+            do
+            {
+                Console.WriteLine("Input Number:");
+                x = Convert.ToDouble(Console.ReadLine());
+                if (x < 0)
+                {
+                    break;
+                }
+                sum += x;
+            } while (true);
+
+            Console.WriteLine("Sum of all number is: "+ sum);
+        }
+    }
+}
